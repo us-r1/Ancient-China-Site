e7cda5c593162a2688e982243f37cb5aa376252f
us-r1 <113575912+us-r1@users.noreply.github.com>
2022-09-15 17:16:49 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+//No Script
