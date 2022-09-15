5f9cc8bd65dd3512bed36c08bbb8a6d3b60181d3
us-r1 <113575912+us-r1@users.noreply.github.com>
2022-09-15 19:17:12 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var siteWidth = 100;
+var siteWidth = 50;
 var scale = screen.width /siteWidth;
 
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
