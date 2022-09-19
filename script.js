f4f71493c2733583f63e235eeeb2b26f34f4fa37
us-r1 <113575912+us-r1@users.noreply.github.com>
2022-09-19 15:18:36 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 var siteWidth = 600;
 var scale = screen.width /siteWidth;
 
+
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
+function AncientChina () {
+    if (document.getElementById('1').style.display == 'none') {
+        document.getElementById('1').style.display = 'block';
+    } else {
+        document.getElementById('1').style.display = 'none';
+    }
+}
+function ImperialChina () {
+    if (document.getElementById('2').style.display == 'none') {
+        document.getElementById('2').style.display = 'block';
+    } else {
+        document.getElementById('2').style.display = 'none';
+    }
+}
+function RepublicofChina () {
+    if (document.getElementById('3').style.display == 'none') {
+        document.getElementById('3').style.display = 'block';
+    } else {
+        document.getElementById('3').style.display = 'none';
+    }
+}
+function ModernChina () {
+    if (document.getElementById('4').style.display == 'none') {
+        document.getElementById('4').style.display = 'block';
+    } else {
+        document.getElementById('4').style.display = 'none';
+    }
+}
